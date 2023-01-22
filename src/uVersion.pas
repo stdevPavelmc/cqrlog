dc80447356739eaec29aaf22b41c610b41c8fe4d
OH1KH <oh1kh@sral.fi>
2023-01-22 10:00:28 +0200

    fixed some toolbar icons

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,7 +4,7 @@ unit uVersion;
 interface
 
 const
-  cVersionBase     = '2.6.0 (108)';
+  cVersionBase     = '2.6.0 (109)';
 
   {$IFDEF LCLGtk2}
   cVERSION    = cVersionBase+' Gtk2';
@@ -21,7 +21,7 @@ const
   cRELEAS     = 0;
   cBUILD      = 1;
 
-  cBUILD_DATE = '2023-01-21';
+  cBUILD_DATE = '2023-01-22';
 
 implementation
 
